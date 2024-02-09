9b075a7caa02030e9b2a4964c8982893cf13ef37
ncriblett <nriblett@mail.pima.edu>
2024-02-09 11:15:43 -0700

    This is a short story about a trip

--- cis129_Lab02_shortStory.py
+++ cis129_Lab02_shortStory.py
@@ -1,1 +1,8 @@
-print('Hello, World!')
+# Nick Riblett
+# My first lab in CIS 129
+# Prints a simple short story
+print('The boys went snowboarding in Colorado.')
+print('They were cruising when they went off the trail.')
+print('''They all got lost and didn't know where to go.''')
+print('Luckily, they found their way back.')
+print('It was the best run of the day and ended the day on a high note.')
