d85b1264689dd09d74822a64cb2d689a26c7427c
ncriblett <nriblett@mail.pima.edu>
2024-03-11 18:35:18 -0700

    FInal Project

--- CIS129_NickRiblett_Lab5.py
+++ CIS129_NickRiblett_Lab5.py
@@ -1,10 +1,31 @@
 #Nick Riblett
 #CIS 129 Lab 5 3/14/24
-#
+# This code gets the total number of bottles for a week and calculates the total earned from the bottles
 
-totalBottles = 0
-counter = 1
-todayBottles = 0
-totalPayout = 0
+#Declared variables
+NBR_OF_DAY= 7
 keepGoing = 'y'
 
+
+while keepGoing == 'y':
+    counter = 1 #Declares variables
+    totalBottles = 0 
+    todayBottles = 0
+    totalPayout = 0
+    while counter <= NBR_OF_DAY: #Gets data for 7 days
+        todayBottles = int(input(f'Enter number of bottles for day #{counter}: '))
+        totalBottles = totalBottles + todayBottles
+        counter += 1
+   
+    totalPayout = totalBottles * 0.10 #gets total payout at 10 cents a bottle
+
+    print()
+    print("The total number of bottles collected is ", totalBottles)
+    print(f"The total paid out is ${totalPayout:.2f}")
+    print()
+
+    print("Do you want to enter another week's worth of data?")
+    keepGoing = input("(Enter y or n): ")
+    print()
+
+          
